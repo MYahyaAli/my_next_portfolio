62db8ee086fe3a4a1b82b74eae71d9fcb6c74474
MYahya <myahya2919@gmail.com>
2024-05-30 00:26:51 +0530

    Mobile Nav and Animations (2nd commit)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,13 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
-const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"], 
-weight:["100","200","300","400","500","600","700","800"],
-variable:"--font-jetbrainsMono", 
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
@@ -16,8 +19,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetBrainsMono.variable}>
-      <Header />
-      {children}</body>
+        <Header />
+        <StairTransition/>
+        <PageTransition>{children}</PageTransition>
+      </body>
     </html>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,39 +1,37 @@
 import Link from "next/link";
-import {Button} from "./ui/button" 
+import { Button } from "./ui/button";
 
 // components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto flex justify-between items-center">
-            {/* logo */}
-            <Link  href="/">
-                <h1 className="text-4xl font-semibold">
-                    M<span className="text-accent">.</span>Yahya
-                </h1>
-            </Link>
-            {/* desktop Nav and hire me button */}
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            M<span className="text-accent">.</span>Yahya
+          </h1>
+        </Link>
+        {/* desktop Nav and hire me button */}
 
-            <div className="hidden xl:flex items-center gap-8">
-            <Nav />
-            <Link href={"/contact"}>
-                <Button>Hire Me</Button>
-            </Link>
-            </div>
-
-            {/* mobile Nav and hire me button */}
-
-            <div className="xl:hidden">
-            <Nav />
-            <Link href={"/contact"}>
-                <Button>Hire Me</Button>
-            </Link>
-            </div>
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href={"/contact"}>
+            <Button>Hire Me</Button>
+          </Link>
         </div>
-    </header>
-  )
-}
 
-export default Header
+        {/* mobile Nav and hire me button */}
+
+        <div className="xl:hidden">
+            <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
