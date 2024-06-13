950773961a925abdca76f717a276212952ebf537
MYahya <myahya2919@gmail.com>
2024-06-13 10:29:50 +0530

    Home page (third commit)

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,61 @@
-export default function Home() {
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <main>HomePage</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+            {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Web Developer</span>
+            <h1 className="h1 mb-6">
+              Hi I am <br />
+              <span className="text-accent"> Mohammad Yahya </span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am a graduate with a BSc in Computer Science (Honors) from the
+              Asia Pacific Institute of Information Technology, in partnership
+              with the University of Staffordshire.{" "}
+              {/*With a strong foundation in
+              web development and a passion for technology.I am eager to
+              contribute my skills and grow professionally. I have experience as
+              a Web Content Writer and Onsite Operation Assistant, which has
+              honed my abilities in both technical and operational domains. My
+              goal is to leverage my expertise in a dynamic company, where I can
+              apply my knowledge, continue learning, and make meaningful
+              contributions to the field of web development. */}
+            </p>
+            {/* button and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span className="">Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
   );
-}
+};
+
+export default Home;
